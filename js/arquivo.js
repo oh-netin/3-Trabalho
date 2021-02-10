7f2cc3aa96d80d07c8cd0124772786a8996058f4
Severino Carvalho da Silva Neto <severinocarvalho14@gmail.com>
2021-02-10 18:16:16 -0300

    Update

--- js/arquivo.js
+++ js/arquivo.js
@@ -1,6 +1,11 @@
+function saveStaticDataToFile() {
+  var blob = new Blob(["Welcome to Websparrow.org."],
+    { type: "text/plain;charset=utf-8" });
+  saveAs(blob, "dados.txt");
+}
 let _retirarMsg = setTimeout(() => {
-    document.getElementById("msg_boasvindas").classList.remove("d-block");
-    document.getElementById("msg_boasvindas").classList.add("d-none");
-    document.getElementById("recarga-jogo").classList.remove('d-none');
-    document.getElementById("recarga-jogo").classList.add('d-block');
-  }, 5000);
+  document.getElementById("msg_boasvindas").classList.remove("d-block");
+  document.getElementById("msg_boasvindas").classList.add("d-none");
+  document.getElementById("recarga-jogo").classList.remove('d-none');
+  document.getElementById("recarga-jogo").classList.add('d-block');
+}, 5000);
